1349a0b1407e71202a5a9a18770c78a0843f0d2f
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-10-02 10:18:54 +0300

    feat: implement SvgIcon.vue logic

--- src/components/ui/SvgIcon/SvgIcon.vue
+++ src/components/ui/SvgIcon/SvgIcon.vue
@@ -1,5 +1,21 @@
-<template></template>
+<template>
+    <svg class="svg_icon" aria-hidden="true" :width="width" :height="height">
+        <use :href="`/travel-map/sprite.svg#${name}`" />
+    </svg>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+    name: { type: String, required: true },
+    width: { type: [String, Number], default: 20 },
+    height: { type: [String, Number], default: 20 },
+});
+</script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.svg_icon {
+    overflow: visible;
+    fill: currentColor;
+    display: inline-block;
+}
+</style>
