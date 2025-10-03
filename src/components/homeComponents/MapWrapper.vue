bd0f0b030d2c8270581c581135edaf52e7cd8f93
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-10-03 19:28:43 +0300

    refactor: move MapWrapper to new folder

---
+++ src/components/homeComponents/MapWrapper.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="world_map_wrapper"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.world_map_wrapper {
+    width: 100%;
+    height: 500px;
+
+    background: red;
+}
+</style>
