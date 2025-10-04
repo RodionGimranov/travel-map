284ee3bad99478c060ca49c3a9f66293ac9176df
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-10-04 20:15:27 +0300

    feat: create Dropdown.vue component

---
+++ src/components/ui/Dropdown/Dropdown.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="dropdown_container" ref="dropdownRef">
+        <div class="country_list">
+            <CountryListItem v-for="country in countries" :key="country.id" :country="country" />
+            <p v-show="countries.length === 0" class="no_result">
+                {{ $t("message.no_result_title") }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { LocalizedCountry } from "@/composables/useLocalizedCountry";
+
+import CountryListItem from "@/components/ui/CountryListItem/CountryListItem.vue";
+
+defineProps<{
+    countries: LocalizedCountry[];
+}>();
+</script>
+
+<style lang="scss">
+.dropdown_container {
+    position: absolute;
+    top: 48px;
+    left: 0;
+    width: 100%;
+    height: 300px;
+    overflow: hidden;
+    transition: 0.2s;
+    border-radius: 12px;
+    padding: 10px 0 10px 10px;
+    background: $primary_white;
+    border: 1px solid $secondary_white;
+    box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.12);
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+}
+
+.country_list {
+    width: 100%;
+    height: 100%;
+    overflow: scroll;
+    padding-right: 10px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+}
+
+.no_result {
+    width: 100%;
+    margin: 20px 0;
+
+    font-size: 16px;
+    font-weight: 400;
+    color: $third_gray;
+    text-align: center;
+}
+</style>
