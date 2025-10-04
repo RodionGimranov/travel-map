b87cfdd2da66ddfc8d7d429ba6a4ca7a7d9179f0
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-10-04 20:17:22 +0300

    build: add pinia import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp, watch } from "vue";
+import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
@@ -7,6 +8,7 @@ import i18n from "@/libs/vue-i18n/vue-i18n";
 import { getPageTitle } from "@/utils/getPageTitle";
 
 const app = createApp(App);
+const pinia = createPinia();
 
 router.beforeEach((to, from, next) => {
     window.scrollTo({ top: 0, behavior: "instant" });
@@ -21,6 +23,6 @@ watch(
     },
 );
 
-app.use(router).use(i18n);
+app.use(router).use(i18n).use(pinia);
 
 app.mount("#app");
