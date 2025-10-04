61b9f91200ffc19090ec3ab98c2827029f967600
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-10-04 20:17:04 +0300

    refactor: update useLocalizedCountry.ts

--- src/composables/useLocalizedCountry.ts
+++ src/composables/useLocalizedCountry.ts
@@ -1,3 +1,5 @@
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
 import countries from "@/data/countries.json";
 
 export interface LocalizedCountry {
@@ -8,13 +10,21 @@ export interface LocalizedCountry {
     name: string;
 }
 
-export const getLocalizedCountries = (locale: "ru" | "en") =>
-    countries.map(
-        (country): LocalizedCountry => ({
-            id: country.id,
-            iso2: country.iso2,
-            iso3: country.iso3,
-            continentId: country.continentId,
-            name: country.name[locale],
-        }),
-    );
+export const getLocalizedCountries = (locale: "ru" | "en"): LocalizedCountry[] =>
+    countries.map((country) => ({
+        id: country.id,
+        iso2: country.iso2,
+        iso3: country.iso3,
+        continentId: country.continentId,
+        name: country.name[locale],
+    }));
+
+export const useLocalizedCountries = () => {
+    const { locale } = useI18n();
+
+    const localizedCountries = computed(() => {
+        return getLocalizedCountries(locale.value as "ru" | "en");
+    });
+
+    return { localizedCountries };
+};
