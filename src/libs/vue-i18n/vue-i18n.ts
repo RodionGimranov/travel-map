5d2ccca10ec52de9e8ee3cd03e1d31f33c74e6c9
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-11-03 21:28:20 +0300

    i18n: update locales

--- src/libs/vue-i18n/vue-i18n.ts
+++ src/libs/vue-i18n/vue-i18n.ts
@@ -2,7 +2,7 @@ import { createI18n } from "vue-i18n";
 
 import globalJson from "@/locales/global.json";
 import countriesStatJson from "@/locales/pages/countriesStat.json";
-import { getLocalizedCountries } from "@/composables/useLocalizedCountry";
+import continentStatJson from "@/locales/pages/continentStat.json";
 
 interface Messages {
     ru: { message: Record<string, any> };
@@ -11,6 +11,7 @@ interface Messages {
 
 const global = globalJson as Messages;
 const countriesStat = countriesStatJson as Messages;
+const continentStat = continentStatJson as Messages;
 
 const i18n = createI18n({
     legacy: false,
@@ -22,14 +23,14 @@ const i18n = createI18n({
             message: {
                 ...global.ru.message,
                 ...countriesStat.ru.message,
-                countries: getLocalizedCountries("ru"),
+                ...continentStat.ru.message,
             },
         },
         en: {
             message: {
                 ...global.en.message,
                 ...countriesStat.en.message,
-                countries: getLocalizedCountries("en"),
+                ...continentStat.en.message,
             },
         },
     },
