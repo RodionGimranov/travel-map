366f02a9f225f8d32e4a89734fd90273f7561960
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-17 20:57:44 +0300

    feat: create SvgIcon.vue

---
+++ src/components/ui/atoms/SvgIcon.vue
@@ -0,0 +1,26 @@
+<template>
+    <svg
+        aria-hidden="true"
+        :width="width"
+        :height="height"
+        class="inline-block overflow-visible fill-current"
+    >
+        <use :href="`/travel-map/sprite.svg#${name}`" />
+    </svg>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        name: string;
+        width: number;
+        height: number;
+    }>(),
+    {
+        width: 20,
+        height: 20,
+    },
+);
+</script>
+
+<style scoped lang="scss"></style>
