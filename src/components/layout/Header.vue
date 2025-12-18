b3357d63bb3d6f8a4cc1cc48a30fe639599a03f7
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-19 00:18:04 +0300

    feat: create Header.vue

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="mb-12.5 flex w-full items-center justify-between">
+        <p class="text-primary-dark text-[18px] font-medium">Travel Map</p>
+        <CountrySearch />
+        <nav class="flex items-center justify-center gap-3"><MenuButton /></nav>
+    </header>
+</template>
+
+<script setup lang="ts">
+import CountrySearch from "@/components/country/CountrySearch.vue";
+import MenuButton from "@/components/ui/atoms/MenuButton.vue";
+</script>
+
+<style lang="scss"></style>
