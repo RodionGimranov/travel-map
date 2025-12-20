d6ce7649bc11784b716170ddf681640c1a795749
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-20 23:45:43 +0300

    refactor: update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<template></template>
+<template>
+    <Header />
+    <SideMenu />
+    <main>
+        <RouterView />
+    </main>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "@/components/layout/Header.vue";
+import SideMenu from "@/components/layout/SideMenu/SideMenu.vue";
+</script>
 
 <style lang="scss"></style>
