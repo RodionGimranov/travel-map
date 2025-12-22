4665987891ef269d323d545a24b10280a0c84409
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-22 17:47:44 +0300

    feat: add languageStore initialization on app mount

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,18 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
+
+import { useLanguageStore } from "@/stores/useLanguageStore";
+
 import Header from "@/components/layout/Header.vue";
 import SideMenu from "@/components/layout/SideMenu/SideMenu.vue";
+
+const languageStore = useLanguageStore();
+
+onMounted(() => {
+    languageStore.init();
+});
 </script>
 
 <style lang="scss"></style>
