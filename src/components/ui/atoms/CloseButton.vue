ae6e5663e21b6e0f39eb0e428185b0241388550f
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-22 17:49:41 +0300

    feat: create UI button components

---
+++ src/components/ui/atoms/CloseButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <button class="hover:bg-secondary-gray flex h-7 w-7 items-center justify-center rounded-lg">
+        <SvgIcon name="cross-icon" :width="20" :height="20" />
+    </button>
+</template>
+
+<script setup lang="ts">
+import SvgIcon from "@/components/ui/atoms/SvgIcon.vue";
+</script>
+
+<style lang="scss"></style>
