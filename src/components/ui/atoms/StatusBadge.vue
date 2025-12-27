b70df63efc9036cd178347e44081c4c3fc446a7d
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-27 12:16:52 +0300

    ui: add StatusBadge component

---
+++ src/components/ui/atoms/StatusBadge.vue
@@ -0,0 +1,40 @@
+<template>
+    <span
+        class="rounded-[100px] px-2! py-0.5! text-center text-[14px] font-normal"
+        :class="badgeType"
+    >
+        {{ label }}
+    </span>
+</template>
+
+<script setup lang="ts">
+type BadgeType = "_gray" | "_blue" | "_orange";
+
+withDefaults(
+    defineProps<{
+        label?: string;
+        badgeType?: BadgeType;
+    }>(),
+    {
+        label: "Label",
+        badgeType: "_gray",
+    },
+);
+</script>
+
+<style lang="scss">
+._gray {
+    background: #1c1301;
+    color: #494846;
+}
+
+._blue {
+    background: #dbeafe;
+    color: #2563eb;
+}
+
+._orange {
+    background: #fed7aa;
+    color: #c2410c;
+}
+</style>
