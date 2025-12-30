f804a09095425e708eb20743ee8cd2ab62b25445
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-30 22:47:00 +0300

    feat: update Header component logic

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,10 +1,25 @@
 <template>
-    <header class="mb-12.5! flex w-full items-center justify-between">
-        <p class="text-primary-dark text-[18px] font-medium">Travel Map</p>
-        <CountrySearch />
-        <nav class="flex items-center justify-center gap-3">
-            <SquareButton :label="buttonLabel" @click="toggleLanguage" />
+    <header class="relative mb-12.5! flex w-full items-center justify-between">
+        <p class="text-primary-dark z-10 text-[18px] font-medium">Travel Map</p>
+        <div class="absolute flex w-full items-center justify-center">
+            <CountrySearch />
+        </div>
+        <nav class="z-10 flex items-center justify-center gap-3">
+            <RouterLink to="/home">
+                <SquareButton
+                    v-if="!$route.meta.hideHomeBtn"
+                    label="common.home_page"
+                    :padding-x="12"
+                />
+            </RouterLink>
             <SquareButton
+                ref="languageButtonRef"
+                :label="buttonLabel"
+                @mousedown.prevent
+                @click="toggleLanguage"
+            />
+            <SquareButton
+                ref="menuButtonRef"
                 icon="menu-icon"
                 :iconSizeW="24"
                 :iconSizeH="24"
@@ -15,7 +30,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 
 import { useLanguageStore } from "@/stores/useLanguageStore";
 import { useSideMenuStore } from "@/stores/useSideMenuStore";
@@ -26,6 +41,9 @@ import SquareButton from "@/components/ui/atoms/SquareButton.vue";
 const languageStore = useLanguageStore();
 const sideMenuStore = useSideMenuStore();
 
+const languageButtonRef = ref<HTMLElement | null>(null);
+const menuButtonRef = ref<HTMLElement | null>(null);
+
 const currentLocale = computed(() => languageStore.locale);
 
 const buttonLabel = computed(() => (currentLocale.value === "ru" ? "Ru" : "En"));
@@ -34,6 +52,11 @@ const toggleLanguage = () => {
     const nextLocale = currentLocale.value === "ru" ? "en" : "ru";
     languageStore.setLocale(nextLocale);
 };
+
+onMounted(() => {
+    sideMenuStore.registerIgnoreOutside(languageButtonRef.value);
+    sideMenuStore.registerIgnoreOutside(menuButtonRef.value);
+});
 </script>
 
 <style lang="scss"></style>
