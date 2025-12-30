327e4afb29abe85e6d20623b07892ebf8b0a556b
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-30 22:50:30 +0300

    refactor: update side menu store

--- src/stores/useSideMenuStore.ts
+++ src/stores/useSideMenuStore.ts
@@ -5,13 +5,13 @@ export type SideMenuType = "settings" | "countries" | null;
 export const useSideMenuStore = defineStore("sideMenu", {
     state: () => ({
         activeMenu: null as SideMenuType,
+        selectedCountryIso2: null as string | null,
+        ignoreOutsideRefs: [] as HTMLElement[],
     }),
 
     getters: {
         isOpen: (state) => state.activeMenu !== null,
-
         isSettingsMenuOpen: (state) => state.activeMenu === "settings",
-
         isCountriesMenuOpen: (state) => state.activeMenu === "countries",
     },
 
@@ -20,12 +20,24 @@ export const useSideMenuStore = defineStore("sideMenu", {
             this.activeMenu = menu;
         },
 
+        openCountry(iso2: string) {
+            this.activeMenu = "countries";
+            this.selectedCountryIso2 = iso2;
+        },
+
         toggle(menu: Exclude<SideMenuType, null>) {
             this.activeMenu = this.activeMenu === menu ? null : menu;
         },
 
         close() {
             this.activeMenu = null;
+            this.selectedCountryIso2 = null;
+        },
+
+        registerIgnoreOutside(el: HTMLElement | null) {
+            if (el && !this.ignoreOutsideRefs.includes(el)) {
+                this.ignoreOutsideRefs.push(el);
+            }
         },
     },
 });
