87c8f9d63dc14b7e2b3332353687b9222667d396
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-01-05 22:02:13 +0300

    types: update continent types

--- src/types/continent.types.ts
+++ src/types/continent.types.ts
@@ -1,9 +1,1 @@
 export type ContinentId = "EU" | "AS" | "AF" | "NA" | "SA" | "OC" | "AN";
-
-export interface Continent {
-    id: ContinentId;
-    name: {
-        ru: string;
-        en: string;
-    };
-}
