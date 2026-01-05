11aa14db6f10c0f9c2d56747ec1d951e2f4a8b02
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-01-05 17:36:46 +0300

    i18n: add new locales

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,16 +1,20 @@
 import ruCommon from "./ru/common.json";
+import ruSideMenu from "./ru/sideMenu.json";
 import ruStats from "./ru/stats.json";
 
 import enCommon from "./en/common.json";
+import enSideMenu from "./en/sideMenu.json";
 import enStats from "./en/stats.json";
 
 export const messages = {
     ru: {
         common: ruCommon,
+        sideMenu: ruSideMenu,
         stats: ruStats,
     },
     en: {
         common: enCommon,
+        sideMenu: enSideMenu,
         stats: enStats,
     },
 };
