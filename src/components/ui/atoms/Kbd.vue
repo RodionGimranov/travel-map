f4cda5c9779fc92c7e0d7ef040b6f2ecd759d639
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-01-08 01:33:02 +0300

    feat: add Kbd component

---
+++ src/components/ui/atoms/Kbd.vue
@@ -0,0 +1,19 @@
+<template>
+    <span
+        class="text-primary-gray bg-secondary-white flex h-5 min-w-5 items-center justify-center overflow-hidden rounded-md px-1! text-[14px] font-medium"
+    >
+        {{ label }}
+    </span>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    label: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    label: "K",
+});
+</script>
+
+<style lang="scss"></style>
