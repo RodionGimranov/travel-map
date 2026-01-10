d1a4601acf6c5168a2bb1d25f33462b3076116bc
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-01-10 20:55:26 +0300

    feat: add DeleteButton component

---
+++ src/components/ui/atoms/DeleteButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <button class="hover:bg-secondary-white flex h-7 w-7 items-center justify-center rounded-lg">
+        <SvgIcon name="trash-icon" :width="18" :height="18" />
+    </button>
+</template>
+
+<script setup lang="ts">
+import SvgIcon from "@/components/ui/atoms/SvgIcon.vue";
+</script>
+
+<style lang="scss"></style>
