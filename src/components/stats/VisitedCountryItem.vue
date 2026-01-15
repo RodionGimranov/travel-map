871849d9b7398e5b55ed625c70028bed53c486b3
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-01-15 17:16:13 +0300

    feat: add VisitedCountryItem component

---
+++ src/components/stats/VisitedCountryItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div
+        class="bg-secondary-white hover:bg-secondary-gray flex max-h-11 min-h-11 w-full cursor-pointer items-center justify-between rounded-xl px-4! py-2.5!"
+        @click="emit('select', iso2)"
+    >
+        <p class="text-primary-dark text-[16px] font-normal">
+            {{ name }}
+        </p>
+        <div class="flex items-center justify-center gap-2">
+            <Badge
+                v-if="countryBadge"
+                :label="countryBadge.label"
+                :badge-type="countryBadge.badgeType"
+            />
+            <DeleteButton @click="emit('remove', iso2)" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { CountryStatus } from "@/types/country.types";
+import { useCountryBadge } from "@/composables/useCountryBadge";
+
+import Badge from "@/components/ui/atoms/Badge.vue";
+import DeleteButton from "@/components/ui/atoms/DeleteButton.vue";
+
+const props = defineProps<{
+    iso2: string;
+    name: string;
+    status: CountryStatus;
+}>();
+
+const emit = defineEmits<{
+    (event: "select", iso2: string): void;
+    (event: "remove", iso2: string): void;
+}>();
+
+const { countryBadge } = useCountryBadge(() => ({
+    status: props.status,
+}));
+</script>
+
+<style lang="scss"></style>
