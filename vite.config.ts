9d4b41b8191c0016e4ef32d27ca5ccc89e21851e
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-01-16 16:54:16 +0300

    feat: add and import transition styles

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
             scss: {
                 additionalData: `
                     @use "@/styles/global" as *;
+                    @use "@/styles/transition" as *;
                 `,
             },
         },
